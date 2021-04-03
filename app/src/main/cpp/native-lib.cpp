7dd7167bfcff8dd7367e5aefc502ee1703243286
nurujjamanpollob <nurujjamanpollob@gmail.com>
2021-04-02 20:44:00 -0700

    updated code

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -27,7 +27,6 @@
 #include <EGL/egl.h>
 #include <GLES/gl.h>
 
-#include <android/sensor.h>
 #include <android/log.h>
 #include <android_native_app_glue.h>
 
@@ -49,9 +48,9 @@ struct saved_state {
 struct engine {
     struct android_app* app;
 
-    ASensorManager* sensorManager;
-    const ASensor* accelerometerSensor;
-    ASensorEventQueue* sensorEventQueue;
+ //   ASensorManager* sensorManager;
+ //   const ASensor* accelerometerSensor;
+//    ASensorEventQueue* sensorEventQueue;
 
     int animating;
     EGLDisplay display;
@@ -233,77 +232,11 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
             // The window is being hidden or closed, clean it up.
             engine_term_display(engine);
             break;
-        case APP_CMD_GAINED_FOCUS:
-            // When our app gains focus, we start monitoring the accelerometer.
-            if (engine->accelerometerSensor != nullptr) {
-                ASensorEventQueue_enableSensor(engine->sensorEventQueue,
-                                               engine->accelerometerSensor);
-                // We'd like to get 60 events per second (in us).
-                ASensorEventQueue_setEventRate(engine->sensorEventQueue,
-                                               engine->accelerometerSensor,
-                                               (1000L/60)*1000);
-            }
-            break;
-        case APP_CMD_LOST_FOCUS:
-            // When our app loses focus, we stop monitoring the accelerometer.
-            // This is to avoid consuming battery while not being used.
-            if (engine->accelerometerSensor != nullptr) {
-                ASensorEventQueue_disableSensor(engine->sensorEventQueue,
-                                                engine->accelerometerSensor);
-            }
-            // Also stop animating.
-            engine->animating = 0;
-            engine_draw_frame(engine);
-            break;
+
+
         default:
             break;
     }
-}
-
-/*
- * AcquireASensorManagerInstance(void)
- *    Workaround ASensorManager_getInstance() deprecation false alarm
- *    for Android-N and before, when compiling with NDK-r15
- */
-#include <dlfcn.h>
-ASensorManager* AcquireASensorManagerInstance(android_app* app) {
-
-    if(!app)
-        return nullptr;
-
-    typedef ASensorManager *(*PF_GETINSTANCEFORPACKAGE)(const char *name);
-    void* androidHandle = dlopen("libandroid.so", RTLD_NOW);
-    auto getInstanceForPackageFunc = (PF_GETINSTANCEFORPACKAGE)
-            dlsym(androidHandle, "ASensorManager_getInstanceForPackage");
-    if (getInstanceForPackageFunc) {
-        JNIEnv* env = nullptr;
-        app->activity->vm->AttachCurrentThread(&env, nullptr);
-
-        jclass android_content_Context = env->GetObjectClass(app->activity->clazz);
-        jmethodID midGetPackageName = env->GetMethodID(android_content_Context,
-                                                       "getPackageName",
-                                                       "()Ljava/lang/String;");
-        auto packageName= (jstring)env->CallObjectMethod(app->activity->clazz,
-                                                         midGetPackageName);
-
-        const char *nativePackageName = env->GetStringUTFChars(packageName, nullptr);
-        ASensorManager* mgr = getInstanceForPackageFunc(nativePackageName);
-        env->ReleaseStringUTFChars(packageName, nativePackageName);
-        app->activity->vm->DetachCurrentThread();
-        if (mgr) {
-            dlclose(androidHandle);
-            return mgr;
-        }
-    }
-
-    typedef ASensorManager *(*PF_GETINSTANCE)();
-    auto getInstanceFunc = (PF_GETINSTANCE)
-            dlsym(androidHandle, "ASensorManager_getInstance");
-    // by all means at this point, ASensorManager_getInstance should be available
-    assert(getInstanceFunc);
-    dlclose(androidHandle);
-
-    return getInstanceFunc();
 }
 
 
@@ -321,15 +254,6 @@ void android_main(struct android_app* state) {
     state->onInputEvent = engine_handle_input;
     engine.app = state;
 
-    // Prepare to monitor accelerometer
-    engine.sensorManager = AcquireASensorManagerInstance(state);
-    engine.accelerometerSensor = ASensorManager_getDefaultSensor(
-            engine.sensorManager,
-            ASENSOR_TYPE_ACCELEROMETER);
-    engine.sensorEventQueue = ASensorManager_createEventQueue(
-            engine.sensorManager,
-            state->looper, LOOPER_ID_USER,
-            nullptr, nullptr);
 
     if (state->savedState != nullptr) {
         // We are starting with a previous saved state; restore from it.
@@ -340,14 +264,14 @@ void android_main(struct android_app* state) {
 
     while (true) {
         // Read all pending events.
-        int ident;
+
         int events;
         struct android_poll_source* source;
 
         // If not animating, we will block forever waiting for events.
         // If animating, we loop until all events are read, then continue
         // to draw the next frame of animation.
-        while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, nullptr, &events,
+        while ((ALooper_pollAll(engine.animating ? 0 : -1, nullptr, &events,
                                       (void**)&source)) >= 0) {
 
             // Process this event.
@@ -355,18 +279,6 @@ void android_main(struct android_app* state) {
                 source->process(state, source);
             }
 
-            // If a sensor has data, process it now.
-            if (ident == LOOPER_ID_USER) {
-                if (engine.accelerometerSensor != nullptr) {
-                    ASensorEvent event;
-                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
-                                                       &event, 1) > 0) {
-                        LOGI("accelerometer: x=%f y=%f z=%f",
-                             event.acceleration.x, event.acceleration.y,
-                             event.acceleration.z);
-                    }
-                }
-            }
 
             // Check if we are exiting.
             if (state->destroyRequested != 0) {
